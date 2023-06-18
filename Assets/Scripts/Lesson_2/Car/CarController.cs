c95fc0df288ea73feb8bb750622d54e775c69d57
platelen <platelen@gmail.com>
2023-06-18 18:38:23 +0300

    Car_02
    1. Добавлен скрипт управления машиной.
    2. Добавлен джойстик для управления машиной.

--- Assets/Scripts/Lesson_2/Car/CarController.cs
+++ Assets/Scripts/Lesson_2/Car/CarController.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class CarController : MonoBehaviour
+namespace Lesson_2.Car
 {
-    // Start is called before the first frame update
-    void Start()
+    public class CarController : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private float _speed = 10f;
+        [SerializeField] private float _rotationSpeed = 100f;
+        [SerializeField] private Joystick _joystick;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Update()
+        {
+            float moveInput = _joystick.Vertical;
+            float rotationInput = _joystick.Horizontal;
+
+            MoveCar(moveInput);
+            RotateCar(rotationInput);
+        }
+
+        private void MoveCar(float moveInput)
+        {
+            Vector3 movement = transform.forward * moveInput * _speed * Time.deltaTime;
+            transform.position += movement;
+        }
+
+        private void RotateCar(float rotationInput)
+        {
+            float rotation = rotationInput * _rotationSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.up, rotation);
+        }
     }
 }
